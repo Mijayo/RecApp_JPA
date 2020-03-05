f6e87d986a51bc6943f5eba7cc7b17f74aa63b92
Diego <48282657+Mijayo@users.noreply.github.com>
2020-03-05 14:01:15 +0100

    _testDiego_V03

--- src/modelo/beans/Eneagrama.java
+++ src/modelo/beans/Eneagrama.java
@@ -3,24 +3,29 @@ package modelo.beans;
 import java.io.Serializable;
 import javax.persistence.*;
 
-
 /**
  * The persistent class for the ENEAGRAMAS database table.
  * 
  */
 @Entity
-@Table(name="ENEAGRAMAS")
-@NamedQuery(name="Eneagrama.findAll", query="SELECT e FROM Eneagrama e")
+@Table(name = "ENEAGRAMAS")
+@NamedQuery(name = "Eneagrama.findAll", query = "SELECT e FROM Eneagrama e")
 public class Eneagrama implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@Column(name="ID_ENEAGRAMA")
+	@Column(name = "ID_ENEAGRAMA")
 	private int idEneagrama;
 
 	private String tipo;
 
 	public Eneagrama() {
+	}
+
+	public Eneagrama(int idEneagrama, String tipo) {
+		super();
+		this.idEneagrama = idEneagrama;
+		this.tipo = tipo;
 	}
 
 	public int getIdEneagrama() {
@@ -38,5 +43,40 @@ public class Eneagrama implements Serializable {
 	public void setTipo(String tipo) {
 		this.tipo = tipo;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + idEneagrama;
+		result = prime * result + ((tipo == null) ? 0 : tipo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Eneagrama other = (Eneagrama) obj;
+		if (idEneagrama != other.idEneagrama)
+			return false;
+		if (tipo == null) {
+			if (other.tipo != null)
+				return false;
+		} else if (!tipo.equals(other.tipo))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Eneagrama [idEneagrama=" + idEneagrama + ", tipo=" + tipo + "]";
+	}
+	
+	
 
 }
--- src/modelo/beans/Usuario.java
+++ src/modelo/beans/Usuario.java
@@ -37,6 +37,20 @@ public class Usuario implements Serializable {
 
 	public Usuario() {
 	}
+	
+	
+
+	public Usuario(int idUsuario, String email, Date fechaAlta, String nombre, String password, Eneagrama eneagrama) {
+		super();
+		this.idUsuario = idUsuario;
+		this.email = email;
+		this.fechaAlta = fechaAlta;
+		this.nombre = nombre;
+		this.password = password;
+		this.eneagrama = eneagrama;
+	}
+
+
 
 	public int getIdUsuario() {
 		return this.idUsuario;
@@ -85,5 +99,72 @@ public class Usuario implements Serializable {
 	public void setEneagrama(Eneagrama eneagrama) {
 		this.eneagrama = eneagrama;
 	}
+
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((eneagrama == null) ? 0 : eneagrama.hashCode());
+		result = prime * result + ((fechaAlta == null) ? 0 : fechaAlta.hashCode());
+		result = prime * result + idUsuario;
+		result = prime * result + ((nombre == null) ? 0 : nombre.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		return result;
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (eneagrama == null) {
+			if (other.eneagrama != null)
+				return false;
+		} else if (!eneagrama.equals(other.eneagrama))
+			return false;
+		if (fechaAlta == null) {
+			if (other.fechaAlta != null)
+				return false;
+		} else if (!fechaAlta.equals(other.fechaAlta))
+			return false;
+		if (idUsuario != other.idUsuario)
+			return false;
+		if (nombre == null) {
+			if (other.nombre != null)
+				return false;
+		} else if (!nombre.equals(other.nombre))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		return true;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Usuario [idUsuario=" + idUsuario + ", email=" + email + ", fechaAlta=" + fechaAlta + ", nombre="
+				+ nombre + ", password=" + password + ", eneagrama=" + eneagrama + "]";
+	}
+	
+	
+	
 
 }
